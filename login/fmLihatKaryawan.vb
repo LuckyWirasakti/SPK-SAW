f89634e3f0e3586809cc3726849e1553b2db30dc
Lucky Wirasakti <lucky.30@students.amikom.ac.id>
2019-12-17 19:44:04 +0700

    save update method and data binding

--- login/fmLihatKaryawan.vb
+++ login/fmLihatKaryawan.vb
@@ -1,6 +1,9 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class fmLihatKaryawan
+
+    Public id As String
+
     Private Sub fmLihatKaryawan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim mysqlco As String
         Dim cmd As MySqlCommand
@@ -22,5 +25,84 @@ Public Class fmLihatKaryawan
 
     Private Sub btnminimize_Click(sender As Object, e As EventArgs) Handles btnminimize.Click
         Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        If e.RowIndex >= 0 Then
+            Dim row As DataGridViewRow
+            row = Me.DataGridView1.Rows(e.RowIndex)
+            fmkaryawan.txtno.Text = row.Cells("id").Value.ToString
+            fmkaryawan.txtnik.Text = row.Cells("nik").Value.ToString
+            fmkaryawan.txtnama.Text = row.Cells("nama").Value.ToString
+            fmkaryawan.tgl_lahir.Text = row.Cells("tgl_lahir").Value.ToString
+            fmkaryawan.txtktlahir.Text = row.Cells("kota_lahir").Value.ToString
+            fmkaryawan.txtagama.Text = row.Cells("agama").Value.ToString
+            fmkaryawan.txtjnskelamin.Text = row.Cells("jns_klmn").Value.ToString
+            fmkaryawan.txtalamat.Text = row.Cells("alamat").Value.ToString
+            fmkaryawan.txtkota.Text = row.Cells("kota").Value.ToString
+            fmkaryawan.txtsuku.Text = row.Cells("suku").Value.ToString
+            fmkaryawan.txtnoktp.Text = row.Cells("no_ktp").Value.ToString
+            fmkaryawan.txttglktp.Text = row.Cells("tgl_berlaku_ktp").Value.ToString
+            fmkaryawan.txtnokk.Text = row.Cells("no_kk").Value.ToString
+            fmkaryawan.txtnonpwp.Text = row.Cells("no_npwp").Value.ToString
+            fmkaryawan.txtnogsm.Text = row.Cells("no_gsm").Value.ToString
+            fmkaryawan.txtemail.Text = row.Cells("email").Value.ToString
+            fmkaryawan.txtibu.Text = row.Cells("nm_ibu").Value.ToString
+            fmkaryawan.txtgoldar.Text = row.Cells("gol_darah").Value.ToString
+            fmkaryawan.txtbpjs.Text = row.Cells("no_bpjs").Value.ToString
+            fmkaryawan.txtbpjs.Text = row.Cells("tgl_bpjs").Value.ToString
+            fmkaryawan.txtjamsostek.Text = row.Cells("no_jamsostek").Value.ToString
+            fmkaryawan.tgl_jamsostek.Text = row.Cells("tgl_jamsostek").Value.ToString
+            fmkaryawan.txtfaskes.Text = row.Cells("kd_faskes").Value.ToString
+            fmkaryawan.txtfaskesgg.Text = row.Cells("kd_faskes_gigi").Value.ToString
+            fmkaryawan.txtstatnikah.Text = row.Cells("sts_nikah").Value.ToString
+            fmkaryawan.tgl_nikah.Text = row.Cells("tgl_nikah").Value.ToString
+            fmkaryawan.txtanak.Text = row.Cells("jml_anak").Value.ToString
+            fmkaryawan.txtssnkeluarga.Text = row.Cells("susunan_klrg").Value.ToString
+            fmkaryawan.txtstattngkerja.Text = row.Cells("sts_tng_krj").Value.ToString
+            fmkaryawan.txtobjekid.Text = row.Cells("objek_id").Value.ToString
+            fmkaryawan.txtposisi.Text = row.Cells("posisi").Value.ToString
+            fmkaryawan.txttitle.Text = row.Cells("tittle").Value.ToString
+            fmkaryawan.txtdirektorat.Text = row.Cells("direktorat").Value.ToString
+            fmkaryawan.txtunit.Text = row.Cells("unit").Value.ToString
+            fmkaryawan.txtsubunit.Text = row.Cells("sub_unit").Value.ToString
+            fmkaryawan.txtsektor.Text = row.Cells("sektor").Value.ToString
+            fmkaryawan.txtrayon.Text = row.Cells("rayon").Value.ToString
+            fmkaryawan.txtidpsa.Text = row.Cells("id_psa").Value.ToString
+            fmkaryawan.txtnmpsa.Text = row.Cells("nm_psa").Value.ToString
+            fmkaryawan.txtwitel.Text = row.Cells("witel").Value.ToString
+            fmkaryawan.txtteritory.Text = row.Cells("teritori").Value.ToString
+            fmkaryawan.txtregional.Text = row.Cells("regional").Value.ToString
+            fmkaryawan.txtstatkerja.Text = row.Cells("sts_kerja").Value.ToString
+            fmkaryawan.txtlamaojt.Text = row.Cells("lama_ojt").Value.ToString
+            fmkaryawan.txtstatinduksi.Text = row.Cells("sts_induksi").Value.ToString
+            fmkaryawan.tgl_mulai_krj.Text = row.Cells("tgl_kerja").Value.ToString
+            fmkaryawan.tgl_kontrak_1.Text = row.Cells("tgl_kontrak_pertama").Value.ToString
+            fmkaryawan.tgl_aw_kontrak.Text = row.Cells("tgl_awal_kontrak").Value.ToString
+            fmkaryawan.tgl_akh_kontrak.Text = row.Cells("tgl_akhir_kontrak").Value.ToString
+            fmkaryawan.txtthnkontrak.Text = row.Cells("tahun_kontrak").Value.ToString
+            fmkaryawan.txtbandpss.Text = row.Cells("band_posisi").Value.ToString
+            fmkaryawan.tgl_band_pss.Text = row.Cells("tgl_band_posisi").Value.ToString
+            fmkaryawan.txtlevel.Text = row.Cells("level").Value.ToString
+            fmkaryawan.tgl_level.Text = row.Cells("tgl_level").Value.ToString
+            fmkaryawan.txtstatpj.Text = row.Cells("sts_pj").Value.ToString
+            fmkaryawan.tgl_stat_pj.Text = row.Cells("tgl_sts_pj").Value.ToString
+            fmkaryawan.tgl_fak_rmh.Text = row.Cells("tgl_fas_rmh").Value.ToString
+            fmkaryawan.tgl_sel_fas.Text = row.Cells("tgl_sel_fas_rmh").Value.ToString
+            fmkaryawan.tgl_aw_nop.Text = row.Cells("tgl_awal_nop").Value.ToString
+            fmkaryawan.tgl_akh_nop.Text = row.Cells("tgl_akhir_nop").Value.ToString
+            fmkaryawan.tgl_aw_cop.Text = row.Cells("tgl_awal_cop").Value.ToString
+            fmkaryawan.tgl_akh_cop.Text = row.Cells("tgl_akhir_cop").Value.ToString
+            fmkaryawan.txtlvlpend.Text = row.Cells("tgl_level_pend").Value.ToString
+            fmkaryawan.txtjurusan.Text = row.Cells("jurusan").Value.ToString
+            fmkaryawan.txtpenypend.Text = row.Cells("peny_pend").Value.ToString
+            fmkaryawan.txtnorek.Text = row.Cells("no_rek").Value.ToString
+            fmkaryawan.txtbank.Text = row.Cells("bank").Value.ToString
+            fmkaryawan.txtcabbank.Text = row.Cells("kantor_cab").Value.ToString
+            fmkaryawan.txtalmtbank.Text = row.Cells("alamat_bank").Value.ToString
+            fmkaryawan.txtnmrek.Text = row.Cells("nama_rek").Value.ToString
+            fmkaryawan.txtpichr.Text = row.Cells("pic_hr").Value.ToString
+            fmkaryawan.txttimezone.Text = row.Cells("time_zone").Value.ToString
+        End If
     End Sub
 End Class
--- login/fmkaryawan.vb
+++ login/fmkaryawan.vb
@@ -58,6 +58,81 @@ Public Class fmkaryawan
         Me.Close()
     End Sub
 
+    Sub kosong()
+        txtnik.Text = ""
+        txtnama.Text = ""
+        tgl_lahir.Text = ""
+        txtktlahir.Text = ""
+        txtagama.Text = ""
+        txtjnskelamin.Text = ""
+        txtalamat.Text = ""
+        txtkota.Text = ""
+        txtsuku.Text = ""
+        txtnoktp.Text = ""
+        txttglktp.Text = ""
+        txtnokk.Text = ""
+        txtnonpwp.Text = ""
+        txtnogsm.Text = ""
+        txtemail.Text = ""
+        txtibu.Text = ""
+        txtgoldar.Text = ""
+        txtbpjs.Text = ""
+        tgl_bpjs.Text = ""
+        txtjamsostek.Text = ""
+        tgl_jamsostek.Text = ""
+        txtfaskes.Text = ""
+        txtfaskesgg.Text = ""
+        txtstatnikah.Text = ""
+        tgl_nikah.Text = ""
+        txtanak.Text = ""
+        txtssnkeluarga.Text = ""
+        txtstattngkerja.Text = ""
+        txtobjekid.Text = ""
+        txtposisi.Text = ""
+        txttitle.Text = ""
+        txtdirektorat.Text = ""
+        txtunit.Text = ""
+        txtsubunit.Text = ""
+        txtsektor.Text = ""
+        txtrayon.Text = ""
+        txtidpsa.Text = ""
+        txtnmpsa.Text = ""
+        txtwitel.Text = ""
+        txtteritory.Text = ""
+        txtregional.Text = ""
+        txtstatkerja.Text = ""
+        txtlamaojt.Text = ""
+        txtstatinduksi.Text = ""
+        tgl_mulai_krj.Text = ""
+        tgl_kontrak_1.Text = ""
+        tgl_aw_kontrak.Text = ""
+        tgl_akh_kontrak.Text = ""
+        txtthnkontrak.Text = ""
+        txtbandpss.Text = ""
+        tgl_band_pss.Text = ""
+        txtlevel.Text = ""
+        tgl_level.Text = ""
+        txtstatpj.Text = ""
+        tgl_stat_pj.Text = ""
+        tgl_fak_rmh.Text = ""
+        tgl_sel_fas.Text = ""
+        tgl_aw_nop.Text = ""
+        tgl_akh_nop.Text = ""
+        tgl_aw_cop.Text = ""
+        tgl_akh_cop.Text = ""
+        txtlvlpend.Text = ""
+        tgl_lv_pend.Text = ""
+        txtjurusan.Text = ""
+        txtpenypend.Text = ""
+        txtnorek.Text = ""
+        txtbank.Text = ""
+        txtcabbank.Text = ""
+        txtalmtbank.Text = ""
+        txtnmrek.Text = ""
+        txtpichr.Text = ""
+        txttimezone.Text = ""
+    End Sub
+
     Private Sub btnsave_Click(sender As Object, e As EventArgs) Handles btnsave.Click
         mysqlco = "server=localhost;user id=root;database=spk;Convert Zero Datetime=True"
         If txtnik.Text = "" Then
@@ -72,79 +147,8 @@ Public Class fmkaryawan
                 read = cmd.ExecuteReader()
                 read.Read()
                 MessageBox.Show("DATA TERSIMPAN")
-                MySql.Close()
-                txtnik.Text = ""
-                txtnama.Text = ""
-                tgl_lahir.Text = ""
-                txtktlahir.Text = ""
-                txtagama.Text = ""
-                txtjnskelamin.Text = ""
-                txtalamat.Text = ""
-                txtkota.Text = ""
-                txtsuku.Text = ""
-                txtnoktp.Text = ""
-                txttglktp.Text = ""
-                txtnokk.Text = ""
-                txtnonpwp.Text = ""
-                txtnogsm.Text = ""
-                txtemail.Text = ""
-                txtibu.Text = ""
-                txtgoldar.Text = ""
-                txtbpjs.Text = ""
-                tgl_bpjs.Text = ""
-                txtjamsostek.Text = ""
-                tgl_jamsostek.Text = ""
-                txtfaskes.Text = ""
-                txtfaskesgg.Text = ""
-                txtstatnikah.Text = ""
-                tgl_nikah.Text = ""
-                txtanak.Text = ""
-                txtssnkeluarga.Text = ""
-                txtstattngkerja.Text = ""
-                txtobjekid.Text = ""
-                txtposisi.Text = ""
-                txttitle.Text = ""
-                txtdirektorat.Text = ""
-                txtunit.Text = ""
-                txtsubunit.Text = ""
-                txtsektor.Text = ""
-                txtrayon.Text = ""
-                txtidpsa.Text = ""
-                txtnmpsa.Text = ""
-                txtwitel.Text = ""
-                txtteritory.Text = ""
-                txtregional.Text = ""
-                txtstatkerja.Text = ""
-                txtlamaojt.Text = ""
-                txtstatinduksi.Text = ""
-                tgl_mulai_krj.Text = ""
-                tgl_kontrak_1.Text = ""
-                tgl_aw_kontrak.Text = ""
-                tgl_akh_kontrak.Text = ""
-                txtthnkontrak.Text = ""
-                txtbandpss.Text = ""
-                tgl_band_pss.Text = ""
-                txtlevel.Text = ""
-                tgl_level.Text = ""
-                txtstatpj.Text = ""
-                tgl_stat_pj.Text = ""
-                tgl_fak_rmh.Text = ""
-                tgl_sel_fas.Text = ""
-                tgl_aw_nop.Text = ""
-                tgl_akh_nop.Text = ""
-                tgl_aw_cop.Text = ""
-                tgl_akh_cop.Text = ""
-                txtlvlpend.Text = ""
-                tgl_lv_pend.Text = ""
-                txtjurusan.Text = ""
-                txtpenypend.Text = ""
-                txtnorek.Text = ""
-                txtbank.Text = ""
-                txtcabbank.Text = ""
-                txtalmtbank.Text = ""
-                txtnmrek.Text = ""
-                txtpichr.Text = ""
-                txttimezone.Text = ""
+                mysql.Close()
+                kosong()
                 Autonumber()
             Catch ex As Exception
                 MessageBox.Show(ex.Message)
@@ -162,10 +166,104 @@ Public Class fmkaryawan
     End Sub
 
     Private Sub btnedit_Click(sender As Object, e As EventArgs) Handles btnedit.Click
-        
+        Try
+            Call buka_koneksi()
+            cmd = New MySqlCommand("UPDATE `karyawan` 
+        SET    `nik` = '" & txtnik.Text & "', 
+               `nama` = '" & txtnama.Text & "', 
+               `tgl_lahir` = '" & tgl_lahir.Text & "', 
+               `kota_lahir` = '" & txtktlahir.Text & "', 
+               `agama` = '" & txtagama.Text & "', 
+               `jns_klmn` = '" & txtjnskelamin.Text & "', 
+               `alamat` = '" & txtalamat.Text & "', 
+               `kota` = '" & txtkota.Text & "', 
+               `suku` = '" & txtsuku.Text & "', 
+               `no_ktp` = '" & txtnoktp.Text & "', 
+               `tgl_berlaku_ktp` = '" & txttglktp.Text & "', 
+               `no_kk` = '" & txtnokk.Text & "', 
+               `no_npwp` = '" & txtnonpwp.Text & "', 
+               `no_gsm` = '" & txtnogsm.Text & "', 
+               `email` = '" & txtemail.Text & "', 
+               `nm_ibu` = '" & txtibu.Text & "', 
+               `gol_darah` = '" & txtgoldar.Text & "', 
+               `no_bpjs` = '" & txtbpjs.Text & "', 
+               `tgl_bpjs` = '" & txtbpjs.Text & "', 
+               `no_jamsostek` = '" & txtjamsostek.Text & "', 
+               `tgl_jamsostek` = '" & tgl_jamsostek.Text & "', 
+               `kd_faskes` = '" & txtfaskes.Text & "', 
+               `kd_faskes_gigi` = '" & txtfaskesgg.Text & "', 
+               `sts_nikah` = '" & txtstatnikah.Text & "', 
+               `tgl_nikah` = '" & tgl_nikah.Text & "', 
+               `jml_anak` = '" & txtanak.Text & "', 
+               `susunan_klrg` = '" & txtssnkeluarga.Text & "', 
+               `sts_tng_krj` = '" & txtstattngkerja.Text & "', 
+               `objek_id` = '" & txtobjekid.Text & "', 
+               `posisi` = '" & txtposisi.Text & "', 
+               `tittle` = '" & txttitle.Text & "', 
+               `direktorat` = '" & txtdirektorat.Text & "', 
+               `unit` = '" & txtunit.Text & "', 
+               `sub_unit` = '" & txtsubunit.Text & "', 
+               `sektor` = '" & txtsektor.Text & "', 
+               `rayon` = '" & txtrayon.Text & "', 
+               `id_psa` = '" & txtidpsa.Text & "', 
+               `nm_psa` = '" & txtnmpsa.Text & "', 
+               `witel` = '" & txtwitel.Text & "', 
+               `teritori` = '" & txtteritory.Text & "', 
+               `regional` = '" & txtregional.Text & "', 
+               `sts_kerja` = '" & txtstatkerja.Text & "', 
+               `lama_ojt` = '" & txtlamaojt.Text & "', 
+               `sts_induksi` = '" & txtstatinduksi.Text & "', 
+               `tgl_kerja` = '" & tgl_mulai_krj.Text & "', 
+               `tgl_kontrak_pertama` = '" & tgl_kontrak_1.Text & "', 
+               `tgl_awal_kontrak` = '" & tgl_aw_kontrak.Text & "', 
+               `tgl_akhir_kontrak` = '" & tgl_akh_kontrak.Text & "', 
+               `tahun_kontrak` = '" & txtthnkontrak.Text & "', 
+               `band_posisi` = '" & txtbandpss.Text & "', 
+               `tgl_band_posisi` = '" & tgl_band_pss.Text & "', 
+               `level` = '" & txtlevel.Text & "', 
+               `tgl_level` = '" & tgl_level.Text & "', 
+               `sts_pj` = '" & txtstatpj.Text & "', 
+               `tgl_sts_pj` = '" & tgl_stat_pj.Text & "', 
+               `tgl_fas_rmh` = '" & tgl_fak_rmh.Text & "', 
+               `tgl_sel_fas_rmh` = '" & tgl_sel_fas.Text & "', 
+               `tgl_awal_nop` = '" & tgl_aw_nop.Text & "', 
+               `tgl_akhir_nop` = '" & tgl_akh_nop.Text & "', 
+               `tgl_awal_cop` = '" & tgl_aw_cop.Text & "', 
+               `tgl_akhir_cop` = '" & tgl_akh_cop.Text & "', 
+               `level_pend` = '" & txtlvlpend.Text & "', 
+               `tgl_level_pend` = '" & tgl_level.Text & "', 
+               `jurusan` = '" & txtjurusan.Text & "', 
+               `peny_pend` = '" & txtpenypend.Text & "', 
+               `no_rek` = '" & txtnorek.Text & "', 
+               `bank` = '" & txtbank.Text & "', 
+               `kantor_cab` = '" & txtcabbank.Text & "', 
+               `alamat_bank` = '" & txtalmtbank.Text & "', 
+               `nama_rek` = '" & txtnmrek.Text & "', 
+               `pic_hr` = '" & txtpichr.Text & "', 
+               `time_zone` = '" & txttimezone.Text & "' 
+        WHERE  id = '" & txtno.Text & "' ", mysqlcon)
+            kosong()
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Data Diperbaharui")
+        Catch ex As Exception
+            MessageBox.Show("Gagal Memperbaharui Data")
+        End Try
     End Sub
 
     Private Sub btnbaru_Click(sender As Object, e As EventArgs) Handles btnbaru.Click
         Autonumber()
+    End Sub
+
+    Private Sub btndelete_Click(sender As Object, e As EventArgs) Handles btndelete.Click
+        Try
+            Call buka_koneksi()
+            cmd = New MySqlCommand("DELETE FROM karyawan WHERE id = '" + txtno.Text + "'", mysqlcon)
+            cmd.ExecuteNonQuery()
+            kosong()
+            MessageBox.Show("Data Terhapus")
+            mysqlcon.Close()
+        Catch ex As Exception
+            MessageBox.Show("Gagal Menghapus Data")
+        End Try
     End Sub
 End Class
